cf11b4a2cac882870f455d1d82d69c0530b539e2
Daniel Demidko <demidko@farpost.com>
2023-07-08 11:39:13 +0600

    deps updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,14 @@ repositories {
 plugins {
   `java-library`
   `maven-publish`
-  kotlin("jvm") version "1.8.10"
+  kotlin("jvm") version "1.9.0"
 }
 dependencies {
   // "api" dependencies is exported to consumers, that is to say found on their compile classpath. 
   // For example, api("org.apache.commons:commons-math3:3.6.1")
   // "implementation" dependencies is private for your library
   // For example, implementation("org.apache.commons:commons-math3:3.6.1")
-  testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
   testImplementation("com.google.truth:truth:1.1.3")
   testImplementation("io.mockk:mockk:1.13.2")
 }
