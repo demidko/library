d297ef9e67cdb82dc8791eef724e4724f855d2d2
Daniil Demidko <demidko@farpost.com>
2025-02-11 23:33:59 -0300

    Global update 2025

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.api.JavaVersion.VERSION_11
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
 
 repositories {
   mavenCentral()
@@ -7,24 +8,26 @@ repositories {
 plugins {
   `java-library`
   `maven-publish`
-  kotlin("jvm") version "1.9.0"
+  kotlin("jvm") version "2.1.20-Beta2"
 }
 dependencies {
-  // "api" dependencies is exported to consumers, that is to say found on their compile classpath. 
+  // "api" dependencies is exported to consumers, that is to say found on their compile classpath.
   // For example, api("org.apache.commons:commons-math3:3.6.1")
   // "implementation" dependencies is private for your library
   // For example, implementation("org.apache.commons:commons-math3:3.6.1")
-  testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
-  testImplementation("com.google.truth:truth:1.1.3")
-  testImplementation("io.mockk:mockk:1.13.2")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+  testImplementation("com.google.truth:truth:1.4.4")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.11.4")
 }
-tasks.withType<KotlinCompile> {
-  kotlinOptions.jvmTarget = "11"
-  kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.time.ExperimentalTime"
+kotlin {
+  compilerOptions {
+    jvmTarget = JVM_11
+    optIn.add("kotlin.time.ExperimentalTime")
+  }
 }
-tasks.withType<JavaCompile> {
-  sourceCompatibility = "11"
-  targetCompatibility = "11"
+java {
+  sourceCompatibility = VERSION_11
+  targetCompatibility = VERSION_11
 }
 tasks.test {
   useJUnitPlatform()
